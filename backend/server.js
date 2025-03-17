0f3a3d8efcbfddf5824958a67de0c47d69765516
unknown <dev.code38@gmail.com>
2025-03-17 10:49:16 +0530

    allowed all origins in CORS

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import path from "path";
+import cors from 'cors';
 
 import { connectDB } from "./config/db.js";
 
@@ -14,6 +15,9 @@ const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
 app.use(express.json()); // allows us to accept JSON data in the req.body
+
+// Allow all origins
+app.use(cors());
 
 app.use("/api/products", productRoutes);
 
